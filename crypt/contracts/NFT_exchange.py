f8b2e4f6a0d5ea0c067d10fc79925c90a2a27acb
Luke Williams <williams.luke.2001@gmail.com>
2023-03-27 17:56:06 -0400

    begun writing NFT exchange contracts

--- crypt/contracts/NFT_exchange.py
+++ crypt/contracts/NFT_exchange.py
@@ -41,3 +41,166 @@ Methods:
         Buys the item for the price.
         Returns the new current bid.
 """
+
+from beaker import *
+from pyteal import *
+from typing import Final
+
+# Base States
+# ===========
+
+class Auction(Application):
+    """State of the auction contract."""
+
+    owner: GlobalStateValue(
+        stack_type=abi.Address,
+        descr="Owner of the contract",
+        default=Global.creator_address()
+    )
+    name: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Name of the item",
+        default=""
+    )
+    image_uri: GlobalStateValue(
+        stack_type=abi.String,
+        descr="URI of the image of the item",
+        default=""
+    )
+    description: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Description of the item",
+        default=""
+    )
+    buy_now_price: GlobalStateValue(
+        stack_type=abi.Uint64,
+        descr="Buy now price of the item",
+        default=0
+    )
+    auction_start_time: GlobalStateValue(
+        stack_type=abi.Uint64,
+        descr="Start time of the auction",
+        default=Global.latest_timestamp()
+    )
+    auction_end_time: GlobalStateValue(
+        stack_type=abi.Uint64,
+        descr="End time of the auction",
+        default=0
+    )
+    auction_start_price: GlobalStateValue(
+        stack_type=abi.Uint64,
+        descr="Start price of the auction",
+        default=0
+    )
+    current_bid: GlobalStateValue(
+        stack_type=abi.Uint64,
+        descr="Current bid of the auction",
+        default=0
+    )
+    current_bidder: GlobalStateValue(
+        stack_type=abi.Address,
+        descr="Current bidder of the auction",
+        default=Global.zero_address()
+    )    
+
+
+class FlatPrice(Application):
+    """State of the flat price contract."""
+
+    owner: GlobalStateValue(
+        stack_type=abi.Address,
+        descr="Owner of the contract",
+        default=Global.creator_address()
+    )
+    name: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Name of the item",
+        default=""
+    )
+    image_uri: GlobalStateValue(
+        stack_type=abi.String,
+        descr="URI of the image of the item",
+        default=""
+    )
+    description: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Description of the item",
+        default=""
+    )
+    price: GlobalStateValue(
+        stack_type=abi.Uint64,
+        descr="Price of the item",
+        default=0
+    )
+
+
+# Specific States
+# ===============
+
+class CardAuction(Auction):
+    """State of the card auction contract."""
+
+    type: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Type of the item",
+        default="card"
+    )
+
+
+class EnemyAuction(Auction):
+    """State of the enemy auction contract."""
+
+    type: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Type of the item",
+        default="enemy"
+    )
+
+
+class CardFlatPrice(FlatPrice):
+    """State of the card flat price contract."""
+
+    type: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Type of the item",
+        default="card"
+    )
+
+
+class EnemyFlatPrice(FlatPrice):
+    """State of the enemy flat price contract."""
+
+    type: GlobalStateValue(
+        stack_type=abi.String,
+        descr="Type of the item",
+        default="enemy"
+    )
+
+
+# Blueprints
+# ==========
+
+def auction_blueprint(app: Application) -> None:
+    @app.create
+    def create() -> Expr:
+        return app.initialize_global_state()
+    
+    @app.external(authorize=Authorize.only(app.owner))
+    def start_auction(
+        paymentTx: abi.PaymentTransaction,
+        name: abi.String,
+        image_uri: abi.String,
+        description: abi.String,
+        auction_end_time: abi.Uint64,
+        auction_start_price: abi.Uint64,
+    ) -> Expr:
+        return Seq(
+            Assert(paymentTx.receiver() == Global.current_application_address()),
+            Assert(paymentTx.amount() == Int(100_000)),
+            app.name.set(name),
+            app.image_uri.set(image_uri),
+            app.description.set(description),
+            app.auction_start_time.set(Global.latest_timestamp()),
+            app.auction_end_time.set(auction_end_time),
+            app.auction_start_price.set(auction_start_price),
+        )
