5e88900451f4b24dce8cf73ef9693a019b66d0c0
shawn-kg <shawnkoshygeorge@gmail.com>
2023-03-29 16:57:17 -0400

    Finished gameflow

--- crypt/game/main.py
+++ crypt/game/main.py
@@ -3,7 +3,7 @@ from models import *
 from engine import *
 
 pygame.init()
-# bounds = (1024, 768)
+# Set Up
 info = pygame.display.Info() 
 screen_width,screen_height = info.current_w,info.current_h
 window_width,window_height = screen_width-10,screen_height-50
@@ -11,41 +11,58 @@ window = pygame.display.set_mode((window_width,window_height))
 algocrypt_icon = pygame.image.load('images/logo.png')
 pygame.display.set_icon(algocrypt_icon)
 pygame.display.set_caption("AlgoCrypt")
+font = pygame.font.SysFont('arial',20, True)
 
-# gameEngine = SnapEngine()
 
-enemyImage = pygame.image.load('images/wizard.png')
-enemyImage = pygame.transform.scale(enemyImage, (int(238*0.8), int(332*0.8)))
+playerDeck = Deck([Card(Types.ATTACK,2,"images/BACK.png")]*30)
+enemyDeck = Deck([Card(Types.ATTACK,2,"images/BACK.png")]*30)
+gameEngine = GameEngine(playerDeck,enemyDeck,
+                        "PalaMAN","Gandalf The Grey",
+                        playerHealth=100,enemyHealth=100,
+                        playerStrength=5,enemyStrength=2,
+                        playerIntelligence=2,enemyIntelligence=10,
+                        playerDexterity=4,enemyDexterity=2,
+                        playerImagePath="images/pala.png",enemyImagePath="images/wizard.png")
+PLAYERHEALTH = gameEngine.player.health
+ENEMYHEALTH = gameEngine.enemy.health
 
-playerImage = pygame.image.load('images/pala.png')
-playerImage = pygame.transform.scale(playerImage, (int(238*0.8), int(332*0.8)))
-
+# Image used for draw and discard piles
 cardBack = pygame.image.load('images/BACK.png')
 cardBack = pygame.transform.scale(cardBack, (int(238*0.6), int(332*0.6)))
 
 def renderGame(window):
   window.fill((1,50,32))
-  font = pygame.font.SysFont('arial',20, True)
+
+  if (gameEngine.state == GameState.STARTUP):
+    startPlayText = font.render("Press The Space Bar To Draw", True, (144, 238, 144))
+    window.blit(startPlayText, (950,450))
 
   #player image
-  window.blit(playerImage, (100, 600))
+  window.blit(gameEngine.player.image, (100, 600))
   #player health
-  pygame.draw.rect(window, (255,0,0), (100, 550, 190, 10)) 
-  pygame.draw.rect(window, (0,128,0), (100, 550, 190, 10)) 
+  healthText = font.render("Health: " + str(gameEngine.player.health) + "/" + str(PLAYERHEALTH), True, (255,0,0), (64,47,29))
+  window.blit(healthText, (300, 825))
+
+  text = font.render(gameEngine.player.name, True, (0,0,0))
+  window.blit(text, (100, 900))
 
   #enemy image
-  window.blit(enemyImage, (100, 100))
+  window.blit(gameEngine.enemy.image, (100, 100))
   #enemy health
-  pygame.draw.rect(window, (255,0,0), (100, 400, 190, 10)) 
-  pygame.draw.rect(window, (0,128,0), (100, 400, 190, 10)) 
+  healthText = font.render("Health: " + str(gameEngine.enemy.health) + "/" + str(ENEMYHEALTH), True, (255,0,0), (64,47,29))
+  window.blit(healthText, (300, 325))
+  text = font.render(gameEngine.enemy.name, True, (0,0,0))
+  window.blit(text, (100, 50))
 
   # Draw piles
+  drawText = font.render("Draw Pile: " + str(gameEngine.player.deck.length()), True, (0,0,255))
+  window.blit(drawText, (500, 800))
   window.blit(cardBack, (500, 600)) # Player
   # Player stats
   userText1 = font.render("User Stats: ", True, (0,0,0))
-  userText2 = font.render("Strength: ", True, (0,0,0))
-  userText3 = font.render("Intelligence: ", True, (0,0,0))
-  userText4 = font.render("Dexterity: ", True, (0,0,0))
+  userText2 = font.render("Strength: " + str(gameEngine.player.strength), True, (0,0,0))
+  userText3 = font.render("Intelligence: " + str(gameEngine.player.intelligence), True, (0,0,0))
+  userText4 = font.render("Dexterity: " + str(gameEngine.player.dexterity), True, (0,0,0))
   window.blit(userText1, (300, 600))
   window.blit(userText2, (300, 650))
   window.blit(userText3, (300, 675))
@@ -54,9 +71,9 @@ def renderGame(window):
   window.blit(cardBack, (500, 100)) # Enemy
   # Enemy stats
   enemyText1 = font.render("Enemy Stats: ", True, (0,0,0))
-  enemyText2 = font.render("Strength: ", True, (0,0,0))
-  enemyText3 = font.render("Intelligence: ", True, (0,0,0))
-  enemyText4 = font.render("Dexterity: ", True, (0,0,0))  
+  enemyText2 = font.render("Strength: " + str(gameEngine.enemy.strength), True, (0,0,0))
+  enemyText3 = font.render("Intelligence: " + str(gameEngine.enemy.intelligence), True, (0,0,0))
+  enemyText4 = font.render("Dexterity: " + str(gameEngine.enemy.dexterity), True, (0,0,0))  
   window.blit(enemyText1, (300, 100))
   window.blit(enemyText2, (300, 150))
   window.blit(enemyText3, (300, 175))
@@ -64,74 +81,76 @@ def renderGame(window):
 
   # Discard piles
   window.blit(cardBack, (1700, 600))
+  discardText = font.render("Discard Pile: " + str(gameEngine.player.discard.length()), True, (0,0,255))
+  window.blit(discardText, (1700, 800))
+
   window.blit(cardBack, (1700, 100))
 
   # Player hand
-  window.blit(cardBack, (800, 700))
-  window.blit(cardBack, (950, 700))
-  window.blit(cardBack, (1100, 700))
-  window.blit(cardBack, (1250, 700))
-  window.blit(cardBack, (1400, 700))
+  if (gameEngine.state == GameState.DRAWING or gameEngine.state == GameState.INPLAY):
+    if (gameEngine.player.hand[0] != None and type(gameEngine.player.hand[0]) == Card):
+      window.blit(gameEngine.player.hand[0].image, (800, 700))
+    if (gameEngine.player.hand[1] != None and type(gameEngine.player.hand[1]) == Card):
+      window.blit(gameEngine.player.hand[1].image, (950, 700))
+    if (gameEngine.player.hand[2] != None and type(gameEngine.player.hand[2]) == Card):
+      window.blit(gameEngine.player.hand[2].image, (1100, 700))
+    if (gameEngine.player.hand[3] != None and type(gameEngine.player.hand[3]) == Card):
+      window.blit(gameEngine.player.hand[3].image, (1250, 700))
+    if (gameEngine.player.hand[4] != None and type(gameEngine.player.hand[4]) == Card):
+      window.blit(gameEngine.player.hand[4].image, (1400, 700))
 
   # Enemy hand
-  window.blit(cardBack, (800, 0))
-  window.blit(cardBack, (950, 0))
-  window.blit(cardBack, (1100, 0))
-  window.blit(cardBack, (1250, 0))
-  window.blit(cardBack, (1400, 0))
+  if (gameEngine.state == GameState.DRAWING or gameEngine.state == GameState.INPLAY):
+    if (gameEngine.enemy.hand[0] != None):
+      window.blit(cardBack, (800, 0))
+    if (gameEngine.enemy.hand[1] != None):
+      window.blit(cardBack, (950, 0))
+    if (gameEngine.enemy.hand[2] != None):
+      window.blit(cardBack, (1100, 0))
+    if (gameEngine.enemy.hand[3] != None):
+      window.blit(cardBack, (1250, 0))
+    if (gameEngine.enemy.hand[4] != None):
+      window.blit(cardBack, (1400, 0))
 
   # Player played cards
-  window.blit(cardBack, (950, 450))
-  window.blit(cardBack, (1100, 450))
-  window.blit(cardBack, (1250, 450))
+  if (gameEngine.state == GameState.DRAWING or gameEngine.state == GameState.INPLAY):
+    if (gameEngine.player.play[0] != None and type(gameEngine.player.play[0]) == Card):
+      window.blit(gameEngine.player.play[0].image, (950, 450))
+    if (gameEngine.player.play[1] != None and type(gameEngine.player.play[1]) == Card):
+      window.blit(gameEngine.player.play[1].image, (1100, 450))
+    if (gameEngine.player.play[2] != None and type(gameEngine.player.play[2]) == Card):
+      window.blit(gameEngine.player.play[2].image, (1250, 450))
 
   # Enemy played cards
-  window.blit(cardBack, (950, 250))
-  window.blit(cardBack, (1100, 250))
-  window.blit(cardBack, (1250, 250))
-  # text = font.render(str(len(gameEngine.player1.hand)) + " cards", True, (0,0,0))
-  # window.blit(text, (100, 500))
-
-  # text = font.render(str(len(gameEngine.player2.hand)) + " cards", True, (255,255,255))
-  # window.blit(text, (700, 500))
-
-  # topCard = gameEngine.pile.peek()
-  # if (topCard != None):
-  #   window.blit(topCard.image, (400, 200))
+  if (gameEngine.state == GameState.INPLAY):
+    if (gameEngine.enemy.play[0] != None):
+      window.blit(gameEngine.enemy.play[0].image, (950, 250))
+    if (gameEngine.enemy.play[1] != None):
+      window.blit(gameEngine.enemy.play[1].image, (1100, 250))
+    if (gameEngine.enemy.play[2] != None):
+      window.blit(gameEngine.enemy.play[2].image, (1250, 250))
 
 
-  # if gameEngine.state == GameState.PLAYING:
-  #   # text = font.render(gameEngine.currentPlayer.name + " to flip", True, (255,255,255))
-  #   # window.blit(text, (20,50))
-  #   pass
-
-  # if gameEngine.state == GameState.SNAPPING:
-  #   result = gameEngine.result
-  #   if result["isSnap"] == True:
-  #     message = "Winning Snap! by " + result["winner"].name
-  #   else:
-  #     message = "False Snap! by " + result["snapCaller"].name + ". " + result["winner"].name + " wins!"
-  #   text = font.render(message, True, (255,255,255))
-  #   window.blit(text, (20,50))
-
-  # if gameEngine.state == GameState.ENDED:
-  #   result = gameEngine.result
-  #   message = "Game Over! " + result["winner"].name + " wins!"
-  #   text = font.render(message, True, (255,255,255))
-  #   window.blit(text, (20,50))
 
 
 run = True
 while run:
   key = None; 
+  if (gameEngine.state == GameState.INPLAY):
+    pygame.time.delay(3000)
+    gameEngine.player.action()
+    gameEngine.enemy.action()
+    gameEngine.state = GameState.STARTUP
+
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
-    if event.type == pygame.KEYDOWN:
+    if (gameEngine.state == GameState.STARTUP or gameEngine.state == GameState.DRAWING) and event.type == pygame.KEYDOWN:
       key = event.key
 
   
-  # gameEngine.play(key)
+  gameEngine.play(key)
   renderGame(window)
   pygame.display.update()
 
--- crypt/game/models.py
+++ crypt/game/models.py
@@ -12,20 +12,21 @@ class Card:
   value = None
   image = None
   
-  def __init__(self, type, value):
+  def __init__(self, type, value, imagePath):
     self.type = type
     self.value = value
-    self.image = pygame.image.load('images/BACK.png')
+    self.image = pygame.image.load(imagePath)
+    self.image = pygame.transform.scale(self.image, (int(238*0.6), int(332*0.6)))
 
 
 class Deck:
   cards = None
   
-  def __init__(self):
-    self.cards = []
-    for suit in Suits:
-      for value in range(1,14):
-        self.cards.append(Card(suit, value))
+  def __init__(self, cards):
+    self.cards = cards
+    # for type in Types:
+    #   for value in range(1,14):
+    #     self.cards.append(Card(type, value))
         
   def shuffle(self):
     random.shuffle(self.cards)
@@ -39,36 +40,131 @@ class Deck:
 
 class Player:
   hand = None
-  flipKey = None
-  snapKey = None
+  play = None
   name = None
+  image = None
+  deck = None
+  discard = Deck([])
+  health = 100
 
-  def __init__(self, name, flipKey, snapKey):
+  strength = 0
+  intelligence = 0
+  dexterity = 0
+
+  def __init__(self, name, deck, health=100, strength=0, intelligence=0, dexterity=0, imagePath=None):
     self.hand = []
-    self.flipKey = flipKey
-    self.snapKey = snapKey
+    self.play = [None] * 3
     self.name = name
+    self.deck = deck
+    self.health = health
+    self.image = pygame.image.load(imagePath)
+    self.image = pygame.transform.scale(self.image, (int(238*0.8), int(332*0.8)))
+
+
+    self.strength = strength
+    self.intelligence = intelligence
+    self.dexterity = dexterity
     
-  def draw(self, deck):
-    self.hand.append(deck.deal())
+  def draw(self):
+    if (self.deck.length() == 0):
+      self.deck = Deck(self.discard.cards)
+      self.discard = Deck([])
+      self.deck.shuffle()
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
     
-  def play(self):
-    return self.hand.pop(0)
+  def playCard(self, cardIndex):
+    if (self.hand[cardIndex] == 0):
+      self.hand[cardIndex] = self.play[0]
+      self.play[0] = None
+      return
+    elif (self.hand[cardIndex] == 1):
+      self.hand[cardIndex] = self.play[1]
+      self.play[1] = None
+      return
+    elif (self.hand[cardIndex] == 2):
+      self.hand[cardIndex] = self.play[2]
+      self.play[2] = None
+      return
+    
+
+    if (self.play[0] == None):
+      self.play[0] = self.hand[cardIndex]
+      self.hand[cardIndex] = 0
+    elif (self.play[1] == None):
+      self.play[1] = self.hand[cardIndex]
+      self.hand[cardIndex] = 1
+    elif (self.play[2] == None):
+      self.play[2] = self.hand[cardIndex]
+      self.hand[cardIndex] = 2
+  
+  def action(self):
+    '''TODO: Implement action logic'''
+    self.discard.cards.append(self.play[0])
+    self.discard.cards.append(self.play[1])
+    self.discard.cards.append(self.play[2])
+
+    self.play = [None] * 3
+    for i in range(5):
+      if (self.hand[i] != 0 and self.hand[i] != 1 and self.hand[i] != 2):
+        self.discard.cards.append(self.hand[i])
+    self.hand.clear()
+      
+  
 
 class Enemy:
   hand = None
-  flipKey = None
-  snapKey = None
+  play = [None] * 3
   name = None
+  image = None
+  deck = None
+  discard = Deck([])
+  health = 100
 
-  def __init__(self, name, flipKey, snapKey):
+  def __init__(self, name, deck, health=100, strength=0, intelligence=0, dexterity=0, imagePath=None ):
     self.hand = []
-    self.flipKey = flipKey
-    self.snapKey = snapKey
     self.name = name
+    self.deck = deck
+    self.health = health
+    self.image  = pygame.image.load(imagePath)
+    self.image = pygame.transform.scale(self.image, (int(238*0.8), int(332*0.8)))
+
+    self.strength = strength
+    self.intelligence = intelligence
+    self.dexterity = dexterity
     
-  def draw(self, deck):
-    self.hand.append(deck.deal())
-    
-  def play(self):
-    return self.hand.pop(0)
+  def draw(self):
+    if (self.deck.length() == 0):
+      self.deck = Deck(self.discard.cards)
+      self.discard = Deck([])
+      self.deck.shuffle()
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
+    self.hand.append(self.deck.deal())
+  
+  def playCard(self):
+    self.play[0] = self.hand[0]
+    self.hand[0] = 0
+
+    self.play[1] = self.hand[1]
+    self.hand[1] = 1
+
+    self.play[2] = self.hand[2]
+    self.hand[2] = 2
+
+  def action(self):
+    '''TODO: Implement action logic'''
+    self.discard.cards.append(self.play[0])
+    self.discard.cards.append(self.play[1])
+    self.discard.cards.append(self.play[2])
+
+    self.play = [None] * 3
+    for i in range(5):
+      if (self.hand[i] != 0 and self.hand[i] != 1 and self.hand[i] != 2):
+        self.discard.cards.append(self.hand[i])
+    self.hand.clear()
